d44b1ffb8aa680bdd44a1cfe81eed2a90ff3e6db
Varp99 <vini.arpini@hotmail.com>
2022-04-18 17:13:45 -0400

    Exercicio da lista

---
+++ Faculdade/Teste.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+peso = float(input("Digite seu peso: "))
+nacionalidade = input("Digite sua nacionalidade: ")
+
+print("Eu me chamo",nome, ",tenho",idade,"anos, Meço",altura,"de altura, Peso",peso,"Kilos e sou",nacionalidade)
