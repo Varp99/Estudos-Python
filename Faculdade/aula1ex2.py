cfee2a6f71284bed5b94a4b222eced1bdf0ce7a4
Varp99 <vini.arpini@hotmail.com>
2022-04-20 18:08:07 -0400

    Atualização

--- Faculdade/aula1ex2.py
+++ Faculdade/aula1ex2.py
@@ -1,8 +1,10 @@
-#Faça um algoritmo que leia a idade de uma pessoa e expressa em dias e mostre-a expressa em anos, meses e dias
+#Faça um algoritmo que leia a idade de uma pessoa expressa em dias e mostre-a expressa em anos, meses e dias
 
 dias = int(input("Digite uma quantidade de dias: "))
 
-idade_em_anos = dias // 365
-idade_em_meses = dias // 30
+idade_em_anos = int(dias / 365)
+dias = dias - idade_em_anos * 365
+idade_em_meses = int(dias / 30)
+dias = dias - idade_em_meses * 30
 
-print(dias, idade_em_meses, idade_em_anos)
+print("Sua idade em anos, meses e dias é respectivamente: ",idade_em_anos,idade_em_meses,dias )
