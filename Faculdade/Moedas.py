101442912dd65fc7f9561483c7471838ba257855
Varp99 <vini.arpini@hotmail.com>
2022-04-21 14:46:45 -0400

    mudança na varial Nmoedas

--- Faculdade/Moedas.py
+++ Faculdade/Moedas.py
@@ -3,26 +3,26 @@ valor = int(input("Digite um valor em centavos: "))
 
 while valor > 0:
     if valor > 0:  
-        Nmoedas = int(valor / 100) 
-        if Nmoedas > 0:
-            print(Nmoedas, "moedas de 1 real")
-            valor = valor - (Nmoedas * 100)
-        Nmoedas = int(valor / 50) 
-        if Nmoedas > 0:
-            print(Nmoedas, "moedas de 50 centavos")
-            valor = valor - (Nmoedas * 50)
-        Nmoedas = int(valor / 25) 
-        if Nmoedas > 0:
-            print(Nmoedas, "moedas de 25 centavos")
-            valor = valor - (Nmoedas * 25)  
-        Nmoedas = int(valor / 10)
-        if Nmoedas > 0:
-            print(Nmoedas, "moedas de 10 centavos")
-            valor = valor - (Nmoedas * 10)
-        Nmoedas = int(valor / 5)
-        if Nmoedas > 0:
-            print(Nmoedas, "moedas de 5 centavos")
-            valor = valor - (Nmoedas * 5)
+        moedas = int(valor / 100) 
+        if moedas > 0:
+            print(moedas, "moedas de 1 real")
+            valor = valor - (moedas * 100)
+        moedas = int(valor / 50) 
+        if moedas > 0:
+            print(moedas, "moedas de 50 centavos")
+            valor = valor - (moedas * 50)
+        moedas = int(valor / 25) 
+        if moedas > 0:
+            print(moedas, "moedas de 25 centavos")
+            valor = valor - (moedas * 25)  
+        moedas = int(valor / 10)
+        if moedas > 0:
+            print(moedas, "moedas de 10 centavos")
+            valor = valor - (moedas * 10)
+        moedas = int(valor / 5)
+        if moedas > 0:
+            print(moedas, "moedas de 5 centavos")
+            valor = valor - (moedas * 5)
         if valor > 0:
             print(valor, "moedas de 1 centavo")
     else:
