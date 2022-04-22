4bb2398861b375e94c2ed55dc298847f766eede3
Varp99 <vini.arpini@hotmail.com>
2022-04-22 10:55:36 -0400

    Calcula a distancia entre dois pontos num plano cartesiano

---
+++ Estudos/Módulo 00 - Faculdade/DistEntreDoisPontos.py
@@ -0,0 +1,12 @@
+#Calcula a distancia entre dois pontos num plano cartesiano.
+
+from math import sqrt
+
+xa = int(input("Insira o valor de XA: "))
+xb = int(input("Insira o valor de XB: "))
+ya = int(input("Insira o valor de YA: "))
+yb = int(input("Insira o valor de YB: "))
+
+dist_AB = sqrt(((xb - xa)**2) + ((yb - ya)**2))
+
+print(dist_AB)
