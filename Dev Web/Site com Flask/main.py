8f4678ae8cb59abba21930dc952a935d2d95dea7
Varp99 <vini.arpini@hotmail.com>
2022-09-24 12:31:39 -0400

    Criou a lista de usuarios e passou ela como parâme

--- Dev Web/Site com Flask/main.py
+++ Dev Web/Site com Flask/main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+lista_usuarios = ['Lira','João']
+
 @app.route("/")  #Caminho da nossa página
 def home():
     return render_template('home.html') #Retorna e renderiza a página home que fica dentro da pasta templates
@@ -9,6 +11,10 @@ def home():
 @app.route("/contato")
 def contato():
     return render_template('contato.html')
+
+@app.route("/usuarios")
+def usuarios():
+    return render_template('usuarios.html', lista_usuarios = lista_usuarios) #Coloca a lista de usuarios como parâmetro aqui para poder chamar ela na página usuarios.html
 
 if __name__ == '__main__':
     app.run(debug=True)
