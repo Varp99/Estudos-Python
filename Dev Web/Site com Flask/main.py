ee5bc6e84a62f39c28ca1297a68bd6bf3e5055bb
Varp99 <vini.arpini@hotmail.com>
2022-09-25 22:00:42 -0400

    Criação dos formulários e segurança deles

--- Dev Web/Site com Flask/main.py
+++ Dev Web/Site com Flask/main.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from lib2to3.pgen2 import token
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 lista_usuarios = ['Lira','João']
+#Token de segurança para formulários
+app.config['SECRET_KEY'] = 'c094b01296fb687d2cd30a591d226ef9'
 
 @app.route("/")  #Caminho da nossa página
 def home():
@@ -15,6 +18,14 @@ def contato():
 @app.route("/usuarios")
 def usuarios():
     return render_template('usuarios.html', lista_usuarios = lista_usuarios) #Coloca a lista de usuarios como parâmetro aqui para poder chamar ela na página usuarios.html
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/criarconta")
+def criar_conta():
+    return render_template('criarconta.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
