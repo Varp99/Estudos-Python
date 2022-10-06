7c4475e3eb91fe091be86fb74a64188afae76097
Varp99 <vini.arpini@hotmail.com>
2022-10-06 16:50:14 -0400

    Correção de erros da atualização passada

--- Dev Web/Site com Flask/main.py
+++ Dev Web/Site com Flask/main.py
@@ -1,6 +1,5 @@
-from crypt import methods
 from lib2to3.pgen2 import token
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormCriarConta, FormLogin
 
 app = Flask(__name__)
@@ -24,11 +23,25 @@ def usuarios():
 @app.route("/login", methods=['GET','POST']) #Toda página que tiver formulário tem que passar esse methods get e post.
 def login():
     form_login = FormLogin()
+
+    if form_login.validate_on_submit() and 'botao_entrar' in request.form: #Verifica se o formulário foi validado e se o botão que eu cliquei foi o de login
+        #Exibir mensagem
+        flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
+        #Redireciona para tal página
+        return redirect(url_for('home'))
+    
     return render_template('login.html', form_login = form_login)
 
 @app.route("/criarconta", methods=['GET','POST'])#Toda página que tiver formulário tem que passar esse methods get e post.
 def criar_conta():
     form_criarconta = FormCriarConta()
+
+    if form_criarconta.validate_on_submit() and 'botao_criar' in request.form: #Verifica se o formulário foi validado e se o botão que eu cliquei foi o de criar conta
+        #Exibir mensagem
+        flash(f'Conta criada com sucesso no e-mail: {form_criarconta.email.data}', 'alert-success')
+        #Redireciona para tal página
+        return redirect(url_for('home'))
+
     return render_template('criarconta.html', form_criarconta = form_criarconta)
 
 if __name__ == '__main__':
