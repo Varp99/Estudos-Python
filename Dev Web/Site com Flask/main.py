bfbe2d3c4ca0c0e2d880824038dc52814622fcde
Varp99 <vini.arpini@hotmail.com>
2022-10-25 17:31:26 -0400

    biblioteca sem utilizar

--- Dev Web/Site com Flask/main.py
+++ Dev Web/Site com Flask/main.py
@@ -1,4 +1,3 @@
-from lib2to3.pgen2 import token
 from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormCriarConta, FormLogin
 from flask_sqlalchemy import SQLAlchemy
