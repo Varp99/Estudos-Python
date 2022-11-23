7d2a30110a1f3f5484501d62018c7a092880c69c
Varp99 <vini.arpini@hotmail.com>
2022-11-23 17:36:39 -0400

    Estudos de listas

--- Coursera/Temperatura.py
+++
@@ -1,6 +0,0 @@
-#Programa para fazer a conversão de Fahreinheit para Celsius
-
-fahreinheit = int(input("Digite a temperatura em Fahreinheit: "))
-
-celsius = (fahreinheit - 32) * 5 / 9
-print("A temperatura em Celsius seria: {} Celsius ".format(celsius))
